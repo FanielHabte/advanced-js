113a3d1f5e494b89059fc4b1546546df00dd25a7
FanielHabte <fanielhabte20@gmail.com>
2023-10-04 15:41:07 -0700

    calc created

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const yearsOfStudy= `2023`
 const goal= `gradauate by 2024`
 
 export{firstName,lastName,yearsOfStudy,goal};
+
+const input =document.querySelector("#degreeC");
+const outPut =document.querySelector(".output");
+const converterBtn = document.querySelector(".calculator");
+
+
+converterBtn.addEventListener("click",()=>{
+   
+   
+    const value = input.value
+    const degreeF = (value -32)*5/9
+       
+    
+
+    outPut.textContent = `${degreeF.toFixed(1)}°C`
+
+
+})
+
+// 32°F − 32) × 5/9 = 0°C
